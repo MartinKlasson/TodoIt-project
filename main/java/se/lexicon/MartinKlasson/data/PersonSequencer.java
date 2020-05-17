198ea43c2bf2c88a448b37fb6adca1ac802b70a5
MartinKlasson <tillmartin2000@yahoo.se>
2020-05-17 11:01:09 +0200

    Updated and finished Sequenser classes

---
+++ main/java/se/lexicon/MartinKlasson/data/PersonSequencer.java
@@ -0,0 +1,14 @@
+package se.lexicon.MartinKlasson.data;
+
+public class PersonSequencer {
+
+    private static int personId = 0;
+
+    public static int nextPersonId(){
+        return ++personId;
+    }
+
+    public static void reset(){
+        personId = 0;
+    }
+}
---
+++ main/java/se/lexicon/MartinKlasson/data/TodoSequencer.java
@@ -0,0 +1,14 @@
+package se.lexicon.MartinKlasson.data;
+
+public class TodoSequencer {
+
+    private static int todoId = 0;
+
+    public static int nextTodoId(){
+        return ++todoId;
+    }
+
+    public static void reset(){
+        todoId = 0;
+    }
+}
